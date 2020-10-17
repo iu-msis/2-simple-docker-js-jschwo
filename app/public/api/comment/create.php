c2025bf41b1cb19097415a84a9a2a84841067af8
Jeremy Schwartz <41627254+jschwo@users.noreply.github.com>
2020-10-17 14:21:47 -0400

    removed id from post

--- app/public/api/comment/create.php
+++ app/public/api/comment/create.php
@@ -12,8 +12,8 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-  'INSERT INTO Comments (id, commentText)
-  VALUES (?, ?)'
+  'INSERT INTO Comments (commentText)
+  VALUES (?)'
 );
 
 
@@ -29,4 +29,4 @@ $stmt->execute([
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../comment/?id=' . $_POST['id']);
+header('Location: ../comment/??id=' . $_POST['id']);
